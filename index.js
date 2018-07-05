9fd841dc3c0321162bc11e957b2cb20f580eaa3d
irshadvali <irshad.vali@gmail.com>
2018-07-05 22:41:19 +0530

    intial

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+export default class AvatarList extends React.Component{
+  render(){
+    <View><Text>
+    irshad
+    </Text>
+    </View>
+  }
+}
